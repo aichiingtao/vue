e66b770f8d9c53c15b8141b2459c3e7fd20de5ca
hwdb1111 <tying8683@gmail.com>
2025-02-28 14:45:17 +0700

    “第一次提交”

---
+++ text/src/App.vue
@@ -0,0 +1,32 @@
+<!-- 加上setup，允许在script直接边写组合式API -->
+<script>
+export default {
+  setup(){
+    const str = '你好'
+
+
+    return{
+      str
+    }
+
+  }
+}
+
+</script>
+
+
+
+<template>
+
+  <div>
+    {{ str }}
+  </div>
+</template>
+
+
+
+
+<style scoped>
+
+
+</style>
---
+++ text/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+//  将创建的实例进行封装，保证每个实例都是独立封闭性
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
